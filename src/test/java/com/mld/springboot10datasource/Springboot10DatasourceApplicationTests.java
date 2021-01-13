8b92a30d24441fa155e0f9c6d8171f58b131fc73
daibiao <db_work@126.com>
2021-01-13 21:50:48 +0800

    redis

--- src/test/java/com/mld/springboot10datasource/Springboot10DatasourceApplicationTests.java
+++ src/test/java/com/mld/springboot10datasource/Springboot10DatasourceApplicationTests.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 
@@ -14,11 +17,26 @@ class Springboot10DatasourceApplicationTests {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
     @Test
     void contextLoads() {
         Long count = jdbcTemplate.queryForObject("select count(1) from user", Long.class);
         System.out.println("count=" + count);
         System.out.println(jdbcTemplate.getDataSource().getClass());
+    }
+
+    @Test
+    void testRedis() {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set("username", "admin");
+        String username = valueOperations.get("username");
+        System.out.println(username);
+        System.out.println(redisConnectionFactory.getClass());
     }
 
 }
